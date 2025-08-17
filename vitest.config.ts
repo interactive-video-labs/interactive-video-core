bf44975b8056a6c30ef72c60e7caa1fcf1770890
Taj <tajulislamj200@gmail.com>
2025-08-18 00:09:25 +0530

    feat: vitest updated

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vitest/config';
+import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
     environment: 'jsdom',
-  },
-});
+    reporters: [
+      'default',
+      ['junit', { outputFile: 'test-report.junit.xml' }]
+    ],
+    outputFile: {
+      junit: 'test-report.junit.xml'
+    },
+    coverage: {
+      provider: 'v8', 
+      reporter: ['text', 'lcov', 'json'],
+      reportsDirectory: './coverage'
+    }
+  }
+})
